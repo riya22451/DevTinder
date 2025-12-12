a5f200d217cfc31c6163c60e5462eae74a99f4de
riya22451 <riyaaggarwal226@gmail.com>
2025-12-12 11:16:59 +0530

    send email functionality

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const BREVO_HOST="smtp-relay.brevo.com"
+export const BREVO_PORT=587
+export const BREVO_USER="9de37f001@smtp-brevo.com"
+export const BREVO_PASS="bskunvsZlzcke7d"
---
+++ src/utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+const { BREVO_HOST, BREVO_PORT, BREVO_USER, BREVO_PASS } = require("./constants");
+
+const sendEmail = async (to, subject, html) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: BREVO_HOST,
+      port: BREVO_PORT,
+      secure: false,
+      auth: {
+        user: BREVO_USER,
+        pass: BREVO_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `DevTinder <${process.env.BREVO_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent successfully!");
+  } catch (error) {
+    console.error("Email sending error:", error);
+  }
+};
+
+module.exports = sendEmail;
